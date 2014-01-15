82a8bbb2b74c838c347ddc95f1bfe56bb76fd39d
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2014-01-15 17:22:38 -0500

    There is still a bug in the reflective boundary conditions. As implemented, they are BC for a slip line (not a no-slip wall). The fluxes at the interfaces should be fine now because those are fixed. However, limiting requires a ghost cell and, right now, in the code, this ghost cell is not implemented correctly I believe. See notes on reflective BC (15/01/14).

--- code/boundaries.cu
+++ code/boundaries.cu
@@ -36,12 +36,10 @@ arch_global void rflctive(int M_s, int M_B, int* boundaryMap, scalar* normals, i
       scalar vyL = UF[((t*N_F+2)*2+0)*M_s+j];
       scalar vxR = invnx2ny2 * ((-nx2+ny2)*vxL +    m2nxny*vyL);
       scalar vyR = invnx2ny2 * (   m2nxny *vxL + (nx2-ny2)*vyL);
-
+      
       // velocities 
-      // UF[((t*N_F+1)*2+1)*M_s+j] = vxR;
-      // UF[((t*N_F+2)*2+1)*M_s+j] = vyR;
-      UF[((t*N_F+1)*2+1)*M_s+j] = UF[((t*N_F+1)*2+1)*M_s+j];
-      UF[((t*N_F+2)*2+1)*M_s+j] =-UF[((t*N_F+2)*2+1)*M_s+j];
+      UF[((t*N_F+1)*2+1)*M_s+j] = vxR;
+      UF[((t*N_F+2)*2+1)*M_s+j] = vyR;
 
 #endif //on dimensions
       
