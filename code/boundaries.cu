e1447adbffdb08376f706ad5f45cced264f553b2
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2014-01-15 15:52:19 -0500

    Testing again on flux...

--- code/boundaries.cu
+++ code/boundaries.cu
@@ -38,8 +38,10 @@ arch_global void rflctive(int M_s, int M_B, int* boundaryMap, scalar* normals, i
       scalar vyR = invnx2ny2 * (   m2nxny *vxL + (nx2-ny2)*vyL);
 
       // velocities 
-      UF[((t*N_F+1)*2+1)*M_s+j] = vxR;
-      UF[((t*N_F+2)*2+1)*M_s+j] = vyR;
+      // UF[((t*N_F+1)*2+1)*M_s+j] = vxR;
+      // UF[((t*N_F+2)*2+1)*M_s+j] = vyR;
+      UF[((t*N_F+1)*2+1)*M_s+j] = UF[((t*N_F+1)*2+1)*M_s+j];
+      UF[((t*N_F+2)*2+1)*M_s+j] =-UF[((t*N_F+2)*2+1)*M_s+j];
 
 #endif //on dimensions
       
