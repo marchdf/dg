5b20ba761d9fcd31f6c372956c82a28bc72f8412
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2014-02-04 15:23:12 -0500

    Minor IC changes.

--- code/init_cond.cc
+++ code/init_cond.cc
@@ -1080,7 +1080,7 @@ void init_dg_rmmulti_multifluid(const int N_s, const int N_E, const fullMatrix<s
   scalar delta=0.005;    // The diffusion layer thickness
     
   // Velocities/pressures in all materials
-  scalar vcoord = 134;//111;//51.5;//134;//72.9; // coordinate shift upwards
+  scalar vcoord = 51.5;//111;//51.5;//134;//72.9; // coordinate shift upwards
   scalar u = 0.0;
   scalar v = 0.0+vcoord;
   scalar p = 1e5;
@@ -1104,10 +1104,10 @@ void init_dg_rmmulti_multifluid(const int N_s, const int N_E, const fullMatrix<s
   scalar M2      = 146.05;//34.76;//
 
   // pre-shock density (material 3)
-  scalar rho03   = 0.1785;//10;//5.494;//10;//
+  scalar rho03   = 10;//0.1785;//10;//5.494;//10;//
   scalar gamma03 = 5.0/3.0;//1.093;//
   scalar alpha03 = 1/(gamma03-1);
-  scalar M3      = 4;//300;//146.05;//300;//
+  scalar M3      = 300;//4;//300;//146.05;//300;//
 
   // Post-shock state (material 1) (see p 101 Toro)
   scalar Ms = 1.21;   // Shock Mach number
