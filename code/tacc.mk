00ad4822cf42b8c127262f673c82cee179a9d491
Marc Henry de Frahan (TACC) <marchdf@umich.edu>
2014-09-05 09:31:21 -0500

    Commiting for backup.

--- code/tacc.mk
+++ code/tacc.mk
@@ -9,9 +9,9 @@ BLAS_LINKER       := -Wl,-rpath,$(TACC_MKL_LIB) -L$(TACC_MKL_LIB) -Wl,--start-gr
 
 LAPACK_INC	  := -I$TACC_MKL_INC
 LAPACK_LIB	  := -L$TACC_MKL_LIB
-LAPACK_LINKER     := -Wl,-rpath,$TACC_MKL_LIB -L$TACC_MKL_LIB -Wl,--start-group -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -Wl,--end-group -lpthread
+LAPACK_LINKER     := -Wl,-rpath,$(TACC_MKL_LIB) -L$(TACC_MKL_LIB) -Wl,--start-group -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -Wl,--end-group -lpthread
 
 GSL_LIB           := $(TACC_GSL_LIB)
 GSL_INC	          := $(TACC_GSL_INC)
 
-CUFLAGS           := -arch=compute_20 -code=sm_20
+CUFLAGS           := -arch=compute_35 -code=sm_35
