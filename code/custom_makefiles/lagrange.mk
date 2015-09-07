3f41006c004e06d31595363cb2869c295dbad1ee
awesome <i.am.brandon.patterson@gmail.com>
2015-09-07 11:01:12 -0400

    Added makefile for lagrange.  Added acoustic wave input to init_cond.cc/.h.s

---
+++ code/custom_makefiles/lagrange.mk
@@ -0,0 +1,18 @@
+##
+## \file lagrange.mk
+## \brief Makefile options for home/lab specific paths
+## \copyright Copyright (C) 2012-2015, Regents of the University of Michigan
+## \license This project is released under the GNU Public License. See LICENSE.
+## \author Marc T. Henry de Frahan <marchdf@umich.edu>, Computational Flow Physics Laboratory, University of Michigan
+## \ingroup makefiles
+##
+## This makefile adjusts file paths for Brandon's lab's computer (currently
+## lagrange.engin.umich.edu).
+##
+##
+## @cond
+
+CUDA_INSTALL_PATH := /usr/local/cuda
+#CUDA_SDK_PATH     := /home/marchdf/NVIDIA_GPU_Computing_SDK/C
+
+## @endcond
