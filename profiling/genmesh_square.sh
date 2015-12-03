e03843a6d80d376b6c6e651e7df2e11f4a333933
Colby Hanley <colbyhanley@0587362111.wireless.umich.net>
2015-12-03 13:05:59 -0500

    commit to share

--- profiling/genmesh_square.sh
+++ profiling/genmesh_square.sh
@@ -4,7 +4,7 @@
 # Generate some square meshes useful for profiling the code. 
 #
 # This script uses sample_square_quad.geo and sample_square_quad.geo
-
+GMSH='/Applications/Gmsh.app/Contents/MacOS/gmsh'
 
 # create the mesh directory
 wrkdir=mesh
@@ -12,7 +12,7 @@ rm -rf $wrkdir;
 mkdir -p $wrkdir;
 
 # different resolutions
-res=(32) #(16 32 64 128 256 512 1024 2048);
+res=(32 64 128 256 ) #(16 32 64 128 256 512 1024 2048);
 
 # number of processors 
 nprocs=(1); 
@@ -27,13 +27,13 @@ for ((i=0; i<${#res[@]}; i++)); do #C style loop with the array length
     # Loop on number of processors
     for ((k=0; k<${#nprocs[@]}; k++)); do #C style loop with the array length
         # Loop on p
-	for ((p=1; p <= 1; p++)); do 
+	for ((p=1; p <= 5; p++)); do 
 
 	    echo ''
 	    echo 'Creating mesh with with p='$p' and N='${res[i]}' and '${nprocs[k]}' partitions';
 	    echo ''
-	    gmsh -2 -order $p -part ${nprocs[k]} my_square_qua.geo -o ./$wrkdir/square_qua_${res[i]}_${p}_np${nprocs[k]}.msh;
-	    gmsh -2 -order $p -part ${nprocs[k]} my_square_tri.geo -o ./$wrkdir/square_tri_${res[i]}_${p}_np${nprocs[k]}.msh;
+	    $GMSH -2 -order $p -part ${nprocs[k]} my_square_qua.geo -o ./$wrkdir/square_qua_${res[i]}_${p}_np${nprocs[k]}.msh;
+	    $GMSH -2 -order $p -part ${nprocs[k]} my_square_tri.geo -o ./$wrkdir/square_tri_${res[i]}_${p}_np${nprocs[k]}.msh;
 	done
     done
     rm -f my_square*.geo;    
