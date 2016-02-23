1549ec0ec88916955d65cc6799a8828c7596aebf
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-02-23 10:49:53 -0500

    Minor change to the way we populate output_time_array. Roundoff error was leading to the last output time not being calculated correctly. This has been fixed.

--- code/src/time_integration/rk.h
+++ code/src/time_integration/rk.h
@@ -8,6 +8,7 @@
 #ifndef RK_H
 #define RK_H
 
+#include <vector>
 #include "macros.h"
 #include "rk_kernels.h"
 #include "kernels.h"
@@ -29,13 +30,17 @@ class RK
   int     _order;  // RK order (only implemented RK4)
   scalar* _beta;
   scalar* _gamma;
+  std::vector<double> _output_time_array;
+  double _Tf;
   
  public:
   /*!
     \brief Constructor
     \param[in] order DG polynomial order
+    \param[in] DtOut output time step
+    \param[in] Tf final time
   */
-  RK(int order) : _order(order){
+  RK(int order, double DtOut, double Tf, const std::vector<double> &output_time_array = std::vector<double>()) : _order(order), _Tf(Tf){
     switch (_order){
     case 4:
       _beta = new scalar[4];
@@ -50,6 +55,35 @@ class RK
       _gamma = new scalar[4];
       _gamma[0] = 1.0/6.0; _gamma[1] = 2.0/6.0; _gamma[2] = 2.0/6.0; _gamma[3] = 1.0/6.0;
     }
+
+    // Calculate the output time array
+    // if the array was not specified in the deck, default to constant DtOut
+    if ((output_time_array.size() == 0) && (DtOut > 0)){
+
+      // Number of ouputs
+      int num_outputs = (int)Tf/DtOut+1; 
+
+      // Populate the array
+      for(int k=0; k < num_outputs; k ++){
+	_output_time_array.push_back(k*DtOut);
+      }
+
+      // Enforce _Tf to be identically exactly the same as the final
+      // time calculated from the for loop that populated the output
+      // time array
+      _Tf = _output_time_array.back(); 
+    }
+    // if it was specified in the deck, use it
+    else if (output_time_array.size() != 0){
+      _output_time_array = output_time_array;
+      _Tf = output_time_array.back();
+    }
+    else{
+      printf("Could not figure out the output times.\nCheck the deck: either specify the output array OR the output delta t\n");
+      exit(1);
+    }
+
+      
   };
 
   /*! Destructor*/
@@ -58,7 +92,7 @@ class RK
     if(_gamma)       delete[] _gamma;
   };
 
-  void RK_integration(double DtOut, double Tf, scalar CFL, int restart_step,
+  void RK_integration(scalar CFL, int restart_step,
 		      int N_E, int N_s, int N_G, int M_T, int M_s, int N_ghosts,
 		      scalar* h_Minv, 
 		      scalar* h_U,
