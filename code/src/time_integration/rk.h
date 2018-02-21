6e7fe68118bd0e94d41fd7e8e55f56fe0b5a5907
suyashtn <suyashtn@umich.edu>
2018-02-21 18:38:48 -0500

    type: src
    RK-TVD implementation and changes made primarily to src/time_integration/rk.h file
    Author: Suyash Tandon
    Date  : Feb 20,2018

--- code/src/time_integration/rk.h
+++ code/src/time_integration/rk.h
@@ -42,12 +42,31 @@ class RK
   */
   RK(int order, double DtOut, double Tf, const std::vector<double> &output_time_array = std::vector<double>()) : _order(order), _Tf(Tf){
     switch (_order){
+    case 2:
+      // This is a case for optimal RK2-TVD method as suggested by
+      // Gottlieb and Shu (1998), Mathematics of Computation
+      _beta = new_scalar[2];
+      _gamma = new_scalar[2];
+      -beta[0] = 0.0; -beta[1] = 1.0;
+      _gamma[0] = 1.0/2.0; _gamma[1] = 1.0/2.0;
+      break;
+      
+    case 3:
+      // This is a case for optimal RK3-TVD method as suggested by
+      // Gottlieb and Shu (1998), Mathematics of Computation
+      _beta = new_scalar[3];
+      _gamma = new_scalar[3];
+      -beta[0] = 0.0; -beta[1] = 1.0; _beta[2] = 0.5;
+      _gamma[0] = 1.0/6.0; _gamma[1] = 1.0/6.0; _gamma[3] = 2.0/3.0;
+      break;
+      
     case 4:
       _beta = new scalar[4];
       _beta[0] = 0.0; _beta[1] = 0.5; _beta[2] = 0.5; _beta[3] = 1.0;
       _gamma = new scalar[4];
       _gamma[0] = 1.0/6.0; _gamma[1] = 2.0/6.0; _gamma[2] = 2.0/6.0; _gamma[3] = 1.0/6.0;
       break;
+      
     default:
       printf("Invalid RK order. Defaulting to RK4.\n");
       _beta = new scalar[4];
